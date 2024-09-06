5243893ae60f3cc75e69c726c701c4571d245f5b
Victor Carvalho <victorcarvalhoadesouza@gmail.com>
2024-09-06 18:28:19 -0300

    Atualização da View e Controller da tela de envio de trabalhos.

--- src/View/TelaEnvioTrabalhoController.java
+++ src/View/TelaEnvioTrabalhoController.java
@@ -1,15 +1,22 @@
 package View;
 
+import com.mycompany.avaliacaosubmissaodetrabalhos.Evento;
 import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
-
+import java.util.EventObject;
+import javafx.stage.FileChooser;
+import java.io.File;
 
 public class TelaEnvioTrabalhoController {
+
+
 
     @FXML
     private Button buttonEnviarTrab;
@@ -30,8 +37,16 @@ public class TelaEnvioTrabalhoController {
     private TextField fieldTitulo;
 
     @FXML
-    void escolherPdfTrab(ActionEvent event) {
-    }
+    private TextField fieldCoAutores;
+
+    @FXML
+    private Label nomeEvento;
+
+    @FXML
+    private Label dataDeAbertura;
+
+    @FXML
+    private Label dataDeVencimento;
 
     @FXML
     void enviarTrabalho(ActionEvent event) {
@@ -39,6 +54,31 @@ public class TelaEnvioTrabalhoController {
         String orientador = fieldOrientador.getText();
         String palavras = fieldPalavrasChave.getText();
         String resumo = fieldResumo.getText();
+        String coAutores = fieldCoAutores.getText();
+    }
+
+    void adicionarTextoLabel(Evento evento){
+        nomeEvento.setText("Evento: " + evento.getNome());
+        dataDeAbertura.setText("Abertura: " + evento.getDataInicio());
+        dataDeVencimento.setText("Vencimento: " + evento.getDataFim());
+    }
+
+    @FXML
+    private void handleAnexarArquivo() {
+        FileChooser fileChooser = new FileChooser();
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PDF files (.pdf)", ".pdf");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        Stage stage = (Stage) buttonEscolhaArquivo.getScene().getWindow(); // Pega a Stage atual
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            System.out.println("Arquivo selecionado: " + file.getAbsolutePath());
+        } else {
+            System.out.println("Nenhum arquivo selecionado.");
+        }
     }
 }
+
 

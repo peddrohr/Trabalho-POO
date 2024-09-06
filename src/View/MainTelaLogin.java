bc27e3ff08be16d04f882364f4092458037b8b9a
Cauan Halison Arantes de Oliveira <129781573+CauanHalison@users.noreply.github.com>
2024-09-05 22:49:31 -0300

    primeira view

---
+++ src/View/MainTelaLogin.java
@@ -0,0 +1,27 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainTelaLogin extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("TelaLogin_1.fxml")));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}

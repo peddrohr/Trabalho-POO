3a4948551ad0320bbf041d2e28331cf489a77f83
Pedro Henrique <royashiauxph@gmail.com>
2024-09-06 00:42:41 -0300

    começo do esboço do menu principal (controller a ser feito ainda)

---
+++ src/View/menuPrincipalUserController.java
@@ -0,0 +1,19 @@
+package View;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+
+public class menuPrincipalUserController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}

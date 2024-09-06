cb032a52a4f7e394493370965675382a795e7b33
Cauan Halison Arantes de Oliveira <129781573+CauanHalison@users.noreply.github.com>
2024-09-05 22:36:57 -0300

    primeira view

--- src/View/TelaLoginController.java
+++ src/View/TelaLoginController.java
@@ -1,26 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package View;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Pedro Henrique
- */
-public class TelaLoginController implements Initializable {
+public class TelaLoginController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    @FXML
+    private Button butaoEntrar;
+
+    @FXML
+    private TextField fieldLogin;
+
+    @FXML
+    private TextField fieldSenha;
+
+    @FXML
+    void validarEntrada(ActionEvent event) {
+        String login = fieldLogin.getText();
+        String senha = fieldSenha.getText();
+
+        System.out.println(senha + login);
+    }
+
 }

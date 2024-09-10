7da0138fed95389df07eb7bda5a3ee3532107f33
César Augusto <ca586772@gmail.com>
2024-09-10 09:06:22 -0300

    comentários das exceções

--- src/com/mycompany/avaliacaosubmissaodetrabalhos/Excecoes/SemTrabalhoDefinidoException.java
+++ src/com/mycompany/avaliacaosubmissaodetrabalhos/Excecoes/SemTrabalhoDefinidoException.java
@@ -1,5 +1,6 @@
 package com.mycompany.avaliacaosubmissaodetrabalhos.Excecoes;
 
+//excecao caso nenhum trabalho seja enviado
 public class SemTrabalhoDefinidoException extends Exception{
     public String getMensagemErro(){
         String mensagem = "Nenhum Trabalho informado";

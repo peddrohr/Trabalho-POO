3b4bee3af38433bc0779869efff398c6fca3e3a6
César Augusto <ca586772@gmail.com>
2024-09-10 12:41:55 -0300

    Update AlunoInvalidoException.java

--- src/com/mycompany/avaliacaosubmissaodetrabalhos/Excecoes/AlunoInvalidoException.java
+++ src/com/mycompany/avaliacaosubmissaodetrabalhos/Excecoes/AlunoInvalidoException.java
@@ -2,7 +2,9 @@ package com.mycompany.avaliacaosubmissaodetrabalhos.Excecoes;
 
 //excecao caso um aluno invalido seja inserido 
 public class AlunoInvalidoException extends Exception{
-    public AlunoInvalidoException(){
-        System.out.print("Aluno inválido, por favor tente novamente");
+    public String getMensagem(){
+        String mensagem = "Aluno inválido, por favor tente novamente";
+        return mensagem;
     }
+    
 }
